9a7359b7d07eef8369f0bffe6f756de14ca87fe1
sushrutpandey029 <102680316+sushrutpandey029@users.noreply.github.com>
2023-02-14 13:35:22 +0530

    First commit

--- src/models/rolesModel.js
+++ src/models/rolesModel.js
@@ -6,9 +6,8 @@ const rolesSchema = new mongoose.Schema({
     fullname: {
         type: String,
         required: true,
-        trim: true
-    },
-
+        trim: true 
+      },
     lastfullname:{
         type: String,
         required: true,
@@ -19,9 +18,10 @@ const rolesSchema = new mongoose.Schema({
         type: String,
         require:true
     },
-
+    
     adminid:{
         type: String,
+        require:true
     },
     
     email: {
@@ -31,8 +31,6 @@ const rolesSchema = new mongoose.Schema({
         match: /^\w+([\.-]?\w+)@\w+([\. -]?\w+)(\.\w{2,3})+$/,
         trim:true
     },
-
-
     Dob:{
         type:String
     },
@@ -44,62 +42,42 @@ const rolesSchema = new mongoose.Schema({
         match: /^(\+91[\-\s]?)?[0]?(91)?[789]\d{9}$/,
         trim:true
     },
-
     password: {
         type: String,
         required: true,
         trim:true,
         
     },
-
     role: {
         type: String,
         enum: ["Admin","Sub-Admin","Doctor"],
         default: "Doctor"
     },
-
     Address:{
         type: String,
     },  
-
     Country:{
         type: String,
-    }, 
-
+    },  
     City:{
         type: String,
-    }, 
-
+    },  
     State:{
         type: String,
     }, 
-
     pincode:{
         type: String,
     },
-
     Biography:{
         type:String
     },
-
     status:{
         type:String
     },
-    
     payment: {
         type: Boolean,
         default: false
-    },
-    isdoctor:{
-        type:String,
-        default:true
-    },
-
-    count:{
-        type:String,
-        default:"0"
-    },
-
+    }
     patientData:[Object],
 },{timestamps:true})
 
